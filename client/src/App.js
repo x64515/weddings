577457842db9d8c478fe2c5a22f551ed631f1872
Xavier <X64515@yahoo.com>
2021-04-05 14:16:01 -0500

    re edited frontend adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
 
-// import Header from './components/Header';
-// import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import Home from "./pages/HomePage";
-// import Login from './pages/Login';
-// import NoMatch from './pages/NoMatch';
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
 // import SingleThought from './pages/SingleThought';
 // import Profile from './pages/Profile';
-// import Signup from './pages/Signup';
+import Signup from "./pages/GuestPortal";
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -37,8 +37,7 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
-              <Route exact path="/profile/:username?" component={Profile} />
-              <Route exact path="/thought/:id" component={SingleThought} /> */}
+
               <Route component={NoMatch} />
             </Switch>
           </div>
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2020 by Lernantino</div>
+      <div className="container">&copy;xavier </div>
     </footer>
   );
 };
