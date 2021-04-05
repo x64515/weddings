1b8be6878c4fc0892050ffa7817c4ad25fd8ab67
j9210 <71297542+j9210@users.noreply.github.com>
2021-04-05 18:54:25 -0500

    remove unnecessary code

--- client/App.js
+++ client/App.js
@@ -37,9 +37,6 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
-              <Route exact path="/profile/:username?" component={Profile} />
-              <Route exact path="/thought/:id" component={SingleThought} />
-
               <Route component={NoMatch} />
             </Switch>
           </div>
