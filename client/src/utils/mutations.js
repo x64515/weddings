3081ccba764ac15f4b979193d127bc74ceb08bfc
Xavier <X64515@yahoo.com>
2021-04-06 13:49:45 -0500

    got drop down menu to work need to change values

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
@@ -24,35 +24,35 @@ export const ADD_USER = gql`
   }
 `;
 
-export const ADD_THOUGHT = gql`
-  mutation addThought($thoughtText: String!) {
-    addThought(thoughtText: $thoughtText) {
-      _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-      }
-    }
-  }
-`;
+// export const ADD_THOUGHT = gql`
+//   mutation addThought($thoughtText: String!) {
+//     addThought(thoughtText: $thoughtText) {
+//       _id
+//       thoughtText
+//       createdAt
+//       username
+//       reactionCount
+//       reactions {
+//         _id
+//       }
+//     }
+//   }
+// `;
 
-export const ADD_REACTION = gql`
-  mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
-    addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
-      _id
-      reactionCount
-      reactions {
-        _id
-        reactionBody
-        createdAt
-        username
-      }
-    }
-  }
-`;
+// export const ADD_REACTION = gql`
+//   mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+//     addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+//       _id
+//       reactionCount
+//       reactions {
+//         _id
+//         reactionBody
+//         createdAt
+//         username
+//       }
+//     }
+//   }
+// `;
 
 export const ADD_FRIEND = gql`
   mutation addFriend($id: ID!) {
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,40 +1,40 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
-export const QUERY_THOUGHTS = gql`
-  query thoughts($username: String) {
-    thoughts(username: $username) {
-      _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-        createdAt
-        username
-        reactionBody
-      }
-    }
-  }
-`;
+// export const QUERY_THOUGHTS = gql`
+//   query thoughts($username: String) {
+//     thoughts(username: $username) {
+//       _id
+//       thoughtText
+//       createdAt
+//       username
+//       reactionCount
+//       reactions {
+//         _id
+//         createdAt
+//         username
+//         reactionBody
+//       }
+//     }
+//   }
+// `;
 
-export const QUERY_THOUGHT = gql`
-  query thought($id: ID!) {
-    thought(_id: $id) {
-      _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-        createdAt
-        username
-        reactionBody
-      }
-    }
-  }
-`;
+// export const QUERY_THOUGHT = gql`
+//   query thought($id: ID!) {
+//     thought(_id: $id) {
+//       _id
+//       thoughtText
+//       createdAt
+//       username
+//       reactionCount
+//       reactions {
+//         _id
+//         createdAt
+//         username
+//         reactionBody
+//       }
+//     }
+//   }
+// `;
 
 export const QUERY_USER = gql`
   query user($username: String!) {
