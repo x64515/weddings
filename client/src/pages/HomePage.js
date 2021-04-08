a9751ec2e4c4cf6a52769eb40d62af90cb54361a
j9210 <71297542+j9210@users.noreply.github.com>
2021-04-07 19:49:40 -0500

    homepage, still needs to be made responsive

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,18 +3,15 @@ import { Link } from "react-router-dom";
 // import coverImage from "../assets/cover/cover-image.jfif";
 function HomePage() {
   return (
-    <section className="my-5">
-      {/* <h1 id="HomePage">My Wedding</h1> */}
-      {/* <img
-        src={coverImage}
-        className="my-2"
-        style={{ width: "100%" }}
-        alt="cover"
-      /> */}
-      <div className="my-2">
-        <Link to="/guestportal">Guest Portal</Link>
-        <Link to="/signup">Signup</Link> 
-      </div>
+    <section className="homepage">
+      <div>
+        <button className="my-2">
+          <Link to="/guestportal">Guest Portal</Link>
+        </button>
+        <button className="my-2"> 
+          <Link to="/signup">Signup</Link> 
+        </button>
+      </div> 
     </section>
   );
 }
