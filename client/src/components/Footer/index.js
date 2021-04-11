8ffc397ca6a087532fd1fe461db26635fecc102d
Xavier <X64515@yahoo.com>
2021-04-11 12:30:25 -0500

    git hub links added to footer styling changed

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,13 +3,13 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer-wrapper">
-      <div className="copy">
-        &copy;
+      <div className="copy">&copy;</div>
+      <div className="developers">
         <a href="http://github.com/x64515">Xavier</a>
-        <a href="http://github.com/">Julia</a>
-        <a href="http://github.com/">Lewis</a>
+        <a href="http://github.com/j9321">Julia</a>
+        <a href="http://github.com/gocus10">Lewis</a>
         <a href="http://github.com/daniela-93">Daniela</a>
-        <a href="http://github.com/">Mike</a>
+        <a href="http://github.com/freklescherokee">Mike</a>
       </div>
     </footer>
   );
