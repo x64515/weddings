857ef8fe0de3323cd6ab1083449b5fa201962295
Xavier <X64515@yahoo.com>
2021-04-11 09:05:14 -0500

    Footer updated 4 1

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer-wrapper">
-      <div className="copy">&copy; Xavier Julia Lewis Daniela Mike </div>
+      <div className="copy">
+        &copy;
+        <a href="http://github.com/x64515">Xavier</a>
+        <a href="http://github.com/">Julia</a>
+        <a href="http://github.com/">Lewis</a>
+        <a href="http://github.com/daniela-93">Daniela</a>
+        <a href="http://github.com/">Mike</a>
+      </div>
     </footer>
   );
 };
